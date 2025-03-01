f63222b57453834e9b80e0073260b1f84baa27f3
Rocket-co-1992 <diogomonteiro1992@outlook.pt>
2025-03-01 21:37:02 +0000

    deleted:    README.md 	deleted:    config/config.php 	deleted:    config/database.php 	deleted:    controllers/AdminController.php 	deleted:    controllers/AuthController.php 	deleted:    controllers/FrontController.php 	deleted:    controllers/PluginController.php 	deleted:    controllers/SectionController.php 	deleted:    controllers/TemplateController.php 	deleted:    controllers/UserController.php 	deleted:    core/Application.php 	deleted:    core/Auth.php 	deleted:    core/Database.php 	deleted:    core/PluginManager.php 	deleted:    core/Router.php 	deleted:    core/SectionManager.php 	deleted:    core/TemplateManager.php 	deleted:    core/bootstrap.php 	deleted:    index.php 	deleted:    templates/default/admin/dashboard.twig 	deleted:    templates/default/admin/layout.twig 	deleted:    templates/default/index.twig

--- README.md
+++
@@ -1,2 +0,0 @@
-# Personal-CMS
-CMS php 8
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'core/bootstrap.php';
-
-// Initialize the application
-$app = new Core\Application();
-
-// Run the application
-$app->run();
